2833c4c0d8b24a3bec0fe2e08690e4c05e19c9d2
Niko <papas000@gmail.com>
2024-06-07 15:01:57 +0300

    ex01

---
+++ ex01/ft_range.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+int *ft_range(int min, int max)
+{
+	int	*range;
+	int	*ptr;
+	int	size;
+	
+	if (min >= max)
+	{
+		return (NULL);
+	}
+	size = max - min;
+	range = (int *)malloc(size * sizeof(int));
+	if (!range)
+	{
+		return (NULL);
+	}
+	ptr = range;
+	while (min < max)
+	{
+		*ptr = min;
+		ptr++;
+		min++;
+	}
+	return (range);
+}
